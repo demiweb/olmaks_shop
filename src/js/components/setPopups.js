21e1b41c17e4b218553cfbc92288c47b08d553d3
yurayarosh <yura12323@gmail.com>
2020-02-21 12:26:54 +0200

    small refactoring

--- src/js/components/setPopups.js
+++ src/js/components/setPopups.js
@@ -1,7 +1,27 @@
 import Popup from 'popup-simple';
-import rotateImage from './rotateImage';
+import '../lib/spritespin';
+
 
 export default class MyPopup extends Popup {
+  setSpinner(action, source) {
+    const $spinner = $(this.popup).find('.js-spinner');
+    if (!$spinner.length) return;
+
+    if (action === 'destroy') {
+      $spinner.spritespin('destroy');    
+    } else {
+      $spinner.spritespin({
+        source,
+        sense: -1,
+        animate: false,
+        plugins: [
+          '360',
+          'drag'
+        ]
+      });
+    } 
+  }
+
   onOpen() {
     if (this.name === 'gallery') {   
       const { srcset } = this.btn.dataset;
@@ -10,13 +30,13 @@ export default class MyPopup extends Popup {
       const gallerySpinnerSrcset = srcset.split(',')
         .map(src => src.trim());
 
-      rotateImage('init', gallerySpinnerSrcset);
+      this.setSpinner('init', gallerySpinnerSrcset);
     }
   }
 
   onClose() {
     if(this.name === 'gallery') {
-      rotateImage('destroy');
+      this.setSpinner('destroy');
     }
   }
 }
